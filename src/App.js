9c06e2cc7bcc10ea91f0aa5080f43a0ded1a5505
songhajang <angpan2004@gmail.com>
2022-10-17 21:15:42 +0900

    coin tracker & 결제 갯수 도출

--- src/App.js
+++ src/App.js
@@ -1,40 +1,58 @@
-const { useState } = require("react");
+import { useEffect } from "react";
+import { useState } from "react";
+// import { flushSync } from "react-dom";
 
 function App() {
-  const [toDo, setToDo] = useState("");
-  const [toDos, setTodos] = useState([]);
-  const onChange = (event) => {
-    setToDo(event.target.value);
-  };
+  const [loading, setLoading] = useState(true);
+  const [coins, setCoins] = useState([]);
+  const [input, setInput] = useState("");
+  const [select, setSelect] = useState("");
+  const [show, setShow] = useState("");
+  useEffect(() => {
+    fetch("https://api.coinpaprika.com/v1/tickers")
+      .then((response) => response.json())
+      .then((json) => {
+        setCoins(json);
+        setLoading(false);
+      });
+  }, []);
+
+  const onInput = (event) => setInput(event.target.value);
+  const onChange = (envent) => setSelect(envent.target.value);
+
   const onSubmit = (event) => {
     event.preventDefault();
-    if (toDo === "") {
-      return;
-    }
-    setTodos((currentArray) => [toDo, ...currentArray]);
-    setToDo("");
+    setInput("");
+    setSelect("");
+    console.log(input, select);
+    setShow(Math.floor(input / select));
   };
-  console.log(toDos);
+
   return (
     <div>
-      <h1>My To Dos :) ({toDos.length})</h1>
+      <h1>The Coins! {loading ? "" : `(${coins.length})`}</h1>
       <form>
         <input
-          onChange={onChange}
-          type="text"
-          placeholder="Write your to do..."
-          value={toDo}
+          value={input}
+          onChange={onInput}
+          type="number"
+          placeholder="금액을 입력해주세요"
         />
-        <button type="submit" onClick={onSubmit}>
-          App To Do
-        </button>
+        {loading ? (
+          <strong>Loading...</strong>
+        ) : (
+          <select onChange={onChange}>
+            {coins.map((coin) => (
+              <option key={coin.id} value={coin.quotes.USD.price}>
+                {coin.name} ({coin.symbol}) : ${coin.quotes.USD.price} USD
+              </option>
+            ))}
+          </select>
+        )}
+        <button onClick={onSubmit}>결제</button>
       </form>
       <hr />
-      <ul>
-        {toDos.map((item, index) => (
-          <li key={index}>{item}</li>
-        ))}
-      </ul>
+      <h1>{show ? `${show}개 살 수 있습니다` : "0개 살 수 있습니다"}</h1>
     </div>
   );
 }
